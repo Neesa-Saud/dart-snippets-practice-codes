e96d1ef6bb7463cf4dbd1c664e7557c0973ce23a
Nisha Saud <nishasaud2061@gmail.com>
2025-01-07 21:55:05 +0545

    break.dart

---
+++ break.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  int i ;
+ for (i =1;i<=10 ;i++)
+ {
+   if(i%2 == 0)
+   {
+     print( i*2) ; 
+   }
+   if( i == 5)
+   {
+     break ; // i ko value 5 vaye vanay it will break
+    }
+ }
+  print ("Loop is stopped");
+}
